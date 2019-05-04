b67e5ac341814f1185ecc6f6467bd02578d58b26
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2019-05-04 10:16:14 +0100

    Quote parameters where COMMON_NAME might include a glob character
    Allows us to issue wildcard certs for Traefik

--- ca/create-certificate.sh
+++ ca/create-certificate.sh
@@ -24,43 +24,43 @@ COMMON_NAME="$1"
 
 cd $IMAGE_DATA/ca/intermediate
 
-if [ ! -e private/$COMMON_NAME.key.pem ]
+if [ ! -e "private/$COMMON_NAME.key.pem" ]
 then
-  openssl genrsa -out private/$COMMON_NAME.key.pem 2048
-  chmod 400 private/$COMMON_NAME.key.pem
+  openssl genrsa -out "private/$COMMON_NAME.key.pem" 2048
+  chmod 400 "private/$COMMON_NAME.key.pem"
 fi
 
-if [ ! -e csr/$COMMON_NAME.csr.pem ]
+if [ ! -e "csr/$COMMON_NAME.csr.pem" ]
 then
   openssl req -config openssl.cnf \
-      -key private/$COMMON_NAME.key.pem \
-      -new -sha256 -out csr/$COMMON_NAME.csr.pem
+      -key "private/$COMMON_NAME.key.pem" \
+      -new -sha256 -out "csr/$COMMON_NAME.csr.pem"
 fi
 
-if [ ! -e certs/$COMMON_NAME.cert.pem ]
+if [ ! -e "certs/$COMMON_NAME.cert.pem" ]
 then
   openssl ca -config openssl.cnf \
       -extensions $EXT \
       -days 365 -notext -md sha256 \
-      -in csr/$COMMON_NAME.csr.pem \
-      -out certs/$COMMON_NAME.cert.pem
-  chmod 444 certs/$COMMON_NAME.cert.pem
+      -in "csr/$COMMON_NAME.csr.pem" \
+      -out "certs/$COMMON_NAME.cert.pem"
+  chmod 444 "certs/$COMMON_NAME.cert.pem"
 fi
 
-openssl x509 -noout -text -in certs/$COMMON_NAME.cert.pem
+openssl x509 -noout -text -in "certs/$COMMON_NAME.cert.pem"
 
-if [ -e dist/$COMMON_NAME.full.pem ]
+if [ -e "dist/$COMMON_NAME.full.pem" ]
 then
-  cat private/$COMMON_NAME.key.pem \
-      certs/$COMMON_NAME.cert.pem \
-      certs/ca-chain.cert.pem > dist/$COMMON_NAME.full.pem
+  cat "private/$COMMON_NAME.key.pem" \
+      "certs/$COMMON_NAME.cert.pem" \
+      certs/ca-chain.cert.pem > "dist/$COMMON_NAME.full.pem"
 fi
 
-if [ -e dist/$COMMON_NAME.full.pfx ]
+if [ -e "dist/$COMMON_NAME.full.pfx" ]
 then
   openssl pkcs12 -export \
-      -inkey private/$COMMON_NAME.key.pem \
-      -in certs/$COMMON_NAME.cert.pem \
+      -inkey "private/$COMMON_NAME.key.pem" \
+      -in "certs/$COMMON_NAME.cert.pem" \
       -certfile certs/ca-chain.cert.pem \
-      -out dist/$COMMON_NAME.full.pfx
+      -out "dist/$COMMON_NAME.full.pfx"
 fi
